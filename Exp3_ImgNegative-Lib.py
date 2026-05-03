15673fb847eb16ad73c8c39d062cf852fd213c1c
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-05-03 21:00:55 +0600

    simplify library code

--- Exp3_ImgNegative-Lib.py
+++ Exp3_ImgNegative-Lib.py
@@ -2,31 +2,29 @@ import cv2
 import matplotlib.pyplot as plt
 
 # Load image
-image = cv2.imread('demo_image.jpg', cv2.IMREAD_UNCHANGED)
+img= cv2.imread('demo_image.jpg')
 
 # Negative image
-img_negative = cv2.bitwise_not(image)
+img_negative = 255 - img
 
 # Prepare for display
-if len(image.shape) == 3:
-    display_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    display_negative = cv2.cvtColor(img_negative, cv2.COLOR_BGR2RGB)
+if len(img.shape) == 3:
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_negative = cv2.cvtColor(img_negative, cv2.COLOR_BGR2RGB)
     cmap = None
 else:
-    display_image = image
-    display_negative = img_negative
     cmap = 'gray'
 
 # Single plotting block
 plt.figure(figsize=(10, 8))
 
 plt.subplot(1, 2, 1)
-plt.imshow(display_image, cmap=cmap)
+plt.imshow(img, cmap=cmap)
 plt.title("Original Image")
 plt.axis("off")
 
 plt.subplot(1, 2, 2)
-plt.imshow(display_negative, cmap=cmap)
+plt.imshow(img_negative, cmap=cmap)
 plt.title("Negative Image")
 plt.axis("off")
 
